bfa3838f3163d640609348774fd519c4feda6056
AngelMoraDev <angelmomaa@gmail.com>
2020-11-04 16:16:54 -0600

    working in events view for simple events model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#home'
-  resources :users, only: [:new, :create, :show]
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create, :show]
+  resources :events
 end
