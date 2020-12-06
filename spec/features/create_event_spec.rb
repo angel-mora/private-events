3e09827af47b8872f6026b2af37a7fb97c95e4a6
AngelMoraDev <angelmomaa@gmail.com>
2020-12-06 14:28:32 -0600

    feature test to create new event failling. Rubocop warnings addressed

--- spec/features/create_event_spec.rb
+++ spec/features/create_event_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
+RSpec.describe Event, type: :feature do
+  # before(:each) do
+  #   assign(:session, Session.new)
+  # end
+
   scenario 'valid inputs' do
     visit new_event_path
-    fill_in 'Event title', with: 'Testing Title'
+    fill_in 'event_title', with: 'Testing Title'
     click_on 'Submit your event'
     visit events_path
     expect(page).to have_content('Testing Title')
