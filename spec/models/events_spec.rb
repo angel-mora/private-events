09c446b312e804a6ad642fc9bd2be80e0cc107ad
AngelMoraDev <angelmomaa@gmail.com>
2020-12-12 11:43:49 -0600

    add model tests for users and events, 7 failures in users and events to be written

---
+++ spec/models/events_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  ###### Events tests ######
+
+  # validates :title, :description, presence: true
+  # validates :title, presence: true
+
+end
+
