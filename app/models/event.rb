f3b7bf245ac74abd184cb2b0023ae283ec99d910
AngelMoraDev <angelmomaa@gmail.com>
2020-12-14 02:55:08 -0600

    complete events, users and invitations model tests and fix rubocop

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :title, :description, presence: true
+  validates :title, :description, :event_time, presence: true, uniqueness: true
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 
   has_many :invitations
